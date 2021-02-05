46638ed5e92c9f8a91f5ca68ad0d4b7c687413ac
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2021-02-05 15:16:44 +0300

    Доработал сборщик по части сборки js

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,24 @@ const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 /* сборка css */
 const sass = require('gulp-sass');
-sass.compiler = require('node-sass');
 /* глобальное подключение всех scss блоков в основной файл стилей  */
 const sassGlob = require('gulp-sass-glob');
+/* генератор карты проекта  */
+const sourcemaps = require('gulp-sourcemaps');
+/* группировка медиа-запросов в стилевых файлах  */
+const gcmq = require('gulp-group-css-media-queries');
+/* минификация css */
+const cleanCSS = require('gulp-clean-css');
+/* автопрефиксер */
+const autoprefixer = require('gulp-autoprefixer');
+/* объединение файлов в один  */
+const concat = require('gulp-concat');
+/* транспиляция js  */
+const babel = require('gulp-babel');
+/* минификация js и исключение неиспользуемого кода */
+const uglify = require('gulp-uglify');
 
+sass.compiler = require('node-sass');
 
 /* таск: удаление папки build + доп аргументов передаем запрет на считывание файлов - это не нужно.*/
 task('clean', () => {
@@ -41,15 +55,37 @@ task('html', () => {
 });
 /* таск: сборка стилей css  */
 task('styles', () => {
-  return src('./src/scss/main.scss')
+  return src(['./node_modules/normalize.css/normalize.css', './src/scss/main.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.css'))
     .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
+    .pipe(gcmq())
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
     .pipe(dest('./build/css/'))
     .pipe(browserSync.reload({ stream: true }));
 });
+/* таск: работа с js  */
+task('scripts', () => {
+  return src(['./src/js/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js', { newLine: ';' }))
+    .pipe(
+      babel({
+        presets: ['@babel/env']
+      }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(dest('./build/js/'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
 /* таск: слежка за изменениями в файлах проекта */
 watch('./src/html/**/*.html', series('html'));
 watch('./src/scss/**/*.scss', series('styles'));
+watch('./src/js/**/*.js', series('scripts'));
 
 
 /* таск: статический сервер для просмотра проекта в браузере  */
@@ -61,4 +97,4 @@ task('server', () => {
   });
 });
 /* основной таск (дефолтный): последовательное выполнение очистка, копирование файлов. */
-task('default', series('clean', 'html', 'styles', 'server'))
+task('default', series('clean', 'html', 'styles', 'scripts', 'server'))
---
+++ src/js/main.js
@@ -0,0 +1,1 @@
+var reflect = value => value;
